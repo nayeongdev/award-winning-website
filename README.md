6d3a6c3cefb728612d58c0617a8018d71c5a6552
nayeongdev <nayeongkim.dev@gmail.com>
2025-04-16 22:04:43 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Awwwards Winning Website
+
+Clone coding with https://youtu.be/zA9r5zTllx4?si=WMwbdGdGT5KwuP6d
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <main>
+      <h1 className="text-3xl font-bold text-blue-600 underline">
+        Welcome to Awwwards
+      </h1>
+    </main>
+  );
+};
+
+export default App;
