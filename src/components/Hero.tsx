0c38efeda05b5bb6b4522416d035bcef81714ab0
nayeongdev <nayeongkim.dev@gmail.com>
2025-04-24 23:53:47 +0900

    feat: Hero 섹션 Background 비디오 추가

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,11 +12,12 @@ export default function Hero() {
   const handleVideoLoad = () => {
     setLoadedVideos((prev) => prev + 1);
   };
-  const upcomingVideoSrc = (currentIndex % totalVideos) + 1;
+
+  const upcomingVideoIndex = (currentIndex % totalVideos) + 1;
 
   const handleMiniVideoClick = () => {
     setHasClicked(true);
-    setCurrentIndex(upcomingVideoSrc);
+    setCurrentIndex(upcomingVideoIndex);
   };
 
   const getVideoSrc = (index: number) => `/videos/hero-${index}.mp4`;
@@ -35,15 +36,34 @@ export default function Hero() {
             >
               <video
                 ref={nextVideoRef}
-                src={getVideoSrc(upcomingVideoSrc)}
+                src={getVideoSrc(upcomingVideoIndex)}
                 loop
                 muted
                 id="current-video"
                 className="size-64 origin-center scale-150 object-cover object-center"
                 onLoadedData={handleVideoLoad}
-              ></video>
+              />
             </div>
           </div>
+          <video
+            ref={nextVideoRef}
+            src={getVideoSrc(currentIndex)}
+            loop
+            muted
+            id="next-video"
+            className="absolute-center invisible absolute z-20 size-64 object-cover object-center"
+            onLoadedData={handleVideoLoad}
+          />
+          <video
+            src={getVideoSrc(
+              currentIndex === totalVideos - 1 ? 1 : currentIndex
+            )}
+            autoPlay
+            loop
+            muted
+            className="absolute top-0 left-0 size-full object-cover object-center"
+            onLoadedData={handleVideoLoad}
+          />
         </div>
       </div>
     </div>
