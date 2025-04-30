7d96508249b22c4867bc23c3e95d1acc27f329c2
nayeongdev <nayeongkim.dev@gmail.com>
2025-04-30 23:46:54 +0900

    feat: About섹션 타이틀 애니메이션 추가

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,6 +1,7 @@
 import gsap from 'gsap';
 
 import { useGSAP } from '@gsap/react';
+import AnimatedTitle from './AnimatedTitle';
 
 import { ScrollTrigger } from 'gsap/all';
 
@@ -32,9 +33,10 @@ const About = () => {
           Welcome to Zentry
         </h2>
 
-        <div className="mt-5 text-center text-4xl leading-[0.8] uppercase md:text-[6rem]">
-          Disc<b>o</b>ver the the world's <br /> l<b>a</b>rgest shared adventure
-        </div>
+        <AnimatedTitle
+          title="Disc<b>o</b>ver the the world's <br /> l<b>a</b>rgest shared adventure"
+          containerClass="mt-5 !text-black text-center"
+        />
 
         <div className="about-subtext">
           <p>The Game of Games begins-your life, now an epic MMORPG</p>
